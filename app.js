1c748c475a5c89a0a729ef6f18c0e8f4fd10050a
Muhammad Muneeb Ali <159451843+MUNEEBALI-DEV@users.noreply.github.com>
2025-06-09 17:29:22 +0500

    made it mobile responsive

--- app.js
+++ app.js
@@ -2,45 +2,52 @@ let input = document.querySelector(".input");
 let button = document.querySelector(".addBtn");
 let lists = document.querySelector(".lists");
 
-
 let i = 1;
 
 function addTaskFunc() {
-  
-    let li = document.createElement("li");
-    let inputTask = document.createElement("input");
-    inputTask.type="text"
-    inputTask.readOnly = true;
-    inputTask.classList.add("toDoInput")
-    inputTask.style.backgroundColor="white"
-    let closeBtn = document.createElement("i");
-    let editBtn = document.createElement("button");
-    editBtn.innerText = "Edit";
-    editBtn.classList.add("editBtn");
-    let saveBtn = document.createElement("button");
-    saveBtn.innerText = "Save";
-    saveBtn.classList.add("editBtn","saveBtn");
-    inputTask.value = input.value;
-    closeBtn.classList.add("fa-solid", "fa-xmark", "closeMark");
-    li.appendChild(inputTask);
-    li.appendChild(editBtn);
-    li.appendChild(saveBtn);
-    li.appendChild(closeBtn);
-    lists.appendChild(li);
-    closeBtnFunc(closeBtn);
-    editBtnFunc(editBtn, inputTask,saveBtn);
-    input.value="";
-  }
+  let li = document.createElement("li");
+  let inputTask = document.createElement("input");
+  inputTask.type = "text";
+  inputTask.readOnly = true;
+  inputTask.classList.add("toDoInput");
+  inputTask.style.backgroundColor = "white";
+  let closeBtn = document.createElement("i");
+  let editBtn = document.createElement("button");
+  editBtn.innerText = "Edit";
+  editBtn.classList.add("editBtn");
+  let saveBtn = document.createElement("button");
+  saveBtn.innerText = "Save";
+  saveBtn.classList.add("editBtn", "saveBtn");
+  inputTask.value = input.value;
+  closeBtn.classList.add("fa-solid", "fa-xmark", "closeMark");
+  li.appendChild(inputTask);
+  li.appendChild(editBtn);
+  li.appendChild(saveBtn);
+  li.appendChild(closeBtn);
+  lists.appendChild(li);
+  closeBtnFunc(closeBtn);
+  editBtnFunc(editBtn, inputTask, saveBtn);
+  input.value = "";
+}
 
+function keypressfunction() {
+  if (!input.value.trim()) {
+    alert("Please write something!!!");
+    return;
+  } else {
+    addTaskFunc();
+  }
+}
 
 button.addEventListener("click", () => {
-if (!input.value.trim()) {
-  alert("Please write something!!!")
-return
-} else {
-  addTaskFunc()
+  keypressfunction();
+})
+
+input.addEventListener("keydown",(e)=>{
+if (e.key=="Enter") {
+  keypressfunction();
 }
-});
+})
 
 function closeBtnFunc(closeBtn) {
   closeBtn.addEventListener("click", () => {
@@ -48,19 +55,18 @@ function closeBtnFunc(closeBtn) {
   });
 }
 
-function editBtnFunc(editBtn, inputTask,saveBtn) {
+function editBtnFunc(editBtn, inputTask, saveBtn) {
   editBtn.addEventListener("click", () => {
-    inputTask.style.backgroundColor="rgb(236, 238, 240)"
+    inputTask.style.backgroundColor = "rgb(236, 238, 240)";
     inputTask.readOnly = false;
     inputTask.focus();
-    saveBtnFunc(saveBtn,inputTask);
+    saveBtnFunc(saveBtn, inputTask);
   });
 }
 
-function saveBtnFunc(saveBtn,inputTask) {
-  saveBtn.addEventListener("click",()=>{
-    inputTask.style.backgroundColor="white"
+function saveBtnFunc(saveBtn, inputTask) {
+  saveBtn.addEventListener("click", () => {
+    inputTask.style.backgroundColor = "white";
     inputTask.readOnly = true;
-
-  })
+  });
 }
